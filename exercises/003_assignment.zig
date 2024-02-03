792693d49cf79c620b82e065385dfba70a932bf0
s0ar <20152463+s-0-a-r@users.noreply.github.com>
2024-02-04 01:44:55 +0900

    Exercises/003 assignment (#3)
    * fix: exercises/003_assignment.zig
    
    - assign new constant
    
    * fix: exercises/003_assignment.zig
    
    - change type of pi to u32
    
    * fix: exercises/003_assignment.zig
    
    - change type of negative_eleven to i8

--- exercises/003_assignment.zig
+++ exercises/003_assignment.zig
@@ -35,11 +35,11 @@ const std = @import("std");
 
 pub fn main() void {
     const n: u8 = 50;
-    n = n + 5;
+    const n_added: u8 = n + 5;
 
-    const pi: u8 = 314159;
+    const pi: u32 = 314159;
 
-    const negative_eleven: u8 = -11;
+    const negative_eleven: i8 = -11;
 
     // There are no errors in the next line, just explanation:
     // Perhaps you noticed before that the print function takes two
@@ -47,5 +47,5 @@ pub fn main() void {
     // is a string. The string may contain placeholders '{}', and the
     // second parameter is an "anonymous list literal" (don't worry
     // about this for now!) with the values to be printed.
-    std.debug.print("{} {} {}\n", .{ n, pi, negative_eleven });
+    std.debug.print("{} {} {}\n", .{ n_added, pi, negative_eleven });
 }
